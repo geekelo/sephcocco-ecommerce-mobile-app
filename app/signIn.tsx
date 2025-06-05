cb32387157ef1d3cda705c4fb96415f837e84c92
nicojoy-gif <orjionyinyechi080@gmail.com>
2025-06-05 09:46:21 +0100

    Integrate enpoint for login

--- app/signIn.tsx
+++ app/signIn.tsx
@@ -20,7 +20,7 @@ export default function SigninScreen() {
 
   const handleLogin = () => {
     if (!email ) {
-      Alert.alert('Missing Fields', 'Please enter email');
+      Alert.alert('Missing Fields', 'Please enter email and password');
       return;
     }
 
@@ -31,6 +31,7 @@ export default function SigninScreen() {
           const { user, token } = data;
           console.log('Login successful:', user, token);
            Alert.alert('User Logged in Successfully');
+          // TODO: Save token in secure storage
           router.push('/storeSelection');
         },
         onError: (error: any) => {
