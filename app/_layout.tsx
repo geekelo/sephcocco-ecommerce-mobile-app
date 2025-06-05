d809c2418e71ac2c460eaeb1232886a4b0ba9672
nicojoy-gif <orjionyinyechi080@gmail.com>
2025-06-05 09:46:21 +0100

    Integrate enpoint for login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,8 @@ import { useFonts } from 'expo-font';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { DarkTheme, DefaultTheme } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/lib/react-query';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -18,8 +20,10 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <QueryClientProvider client={queryClient}>
       <Slot />
       <StatusBar style="auto" />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
